b06b4aab19f3d925a3af89583fca45c573088148
iYunghui <betty103800@gmail.com>
2022-05-08 15:08:01 +0800

    test 2

--- apps/potter/src/app/potter.spec.ts
+++ apps/potter/src/app/potter.spec.ts
@@ -35,5 +35,26 @@ describe('Potter', () => {
     let books = [1, 1, 1];
     expect(potter.calculatePrice(books)).toBe(8 * 3);
   });
+
+  test('testSimpleDiscounts', () => {
+    const potter = new Potter();
+    let books = [0, 1];
+    expect(potter.calculatePrice(books)).toBe(8 * 2 * 0.95);
+  });
+  test('testSimpleDiscounts', () => {
+    const potter = new Potter();
+    let books = [0, 2, 4];
+    expect(potter.calculatePrice(books)).toBe(8 * 3 * 0.9);
+  });
+  test('testSimpleDiscounts', () => {
+    const potter = new Potter();
+    let books = [0, 1, 2, 4];
+    expect(potter.calculatePrice(books)).toBe(8 * 4 * 0.8);
+  });
+  test('testSimpleDiscounts', () => {
+    const potter = new Potter();
+    let books = [0, 1, 2, 3, 4];
+    expect(potter.calculatePrice(books)).toBe(8 * 5 * 0.75);
+  });
 });
 
